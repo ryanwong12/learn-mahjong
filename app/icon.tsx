e64a4e3f69c779e23c38e894313c0f1b5ecc0aa9
ryanwong2000 <ryanwong2000@hotmail.com>
2025-07-15 20:21:01 -0400

    metadata

---
+++ app/icon.tsx
@@ -0,0 +1,28 @@
+// src/app/icon.tsx
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          // background: "linear-gradient(90deg, #10b981 0%, #3b82f6 100%)",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          // color: "white",
+        }}
+      >
+        🀄
+      </div>
+    ),
+    { ...size }
+  );
+}
