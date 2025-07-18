6e5a76515fda317f4ca802a5f4fb9b11be32ed5c
Ryan Wong <ryanwong2000@hotmail.com>
2025-07-17 23:40:30 -0400

    add pwa icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#10b981',
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icon',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        src: '/icon',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512.png',
+        src: '/icon',
         sizes: '512x512',
         type: 'image/png',
       },
