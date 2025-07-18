a6d2c0a0e7056c0942d56c050ce90e754a9f45eb
Ryan Wong <ryanwong2000@hotmail.com>
2025-07-17 23:35:30 -0400

    use Image tag

--- components/mahjong/TileDisplay.tsx
+++ components/mahjong/TileDisplay.tsx
@@ -1,13 +1,16 @@
 import MahjongTile from "@/types/MahjongTile";
+import Image from 'next/image'
 
 const TileDisplay = ({ tile }: { tile: MahjongTile }) => (
   <div className="tile-container flex justify-center">
     {/* Try Unicode first */}
     {/* <span className="unicode-tile">{tile.unicode}</span> */}
     {/* SVG fallback */}
-    <img
+    <Image
       src={`/tiles/svg/${tile.category}/${tile.id}.svg`}
       alt={tile.nameEnglish}
+      width={100}
+      height={100}
     />
   </div>
 );
